53b8c915eb8bb0d0871a1d37e6492ec07de0de0d
K0unty <kountiapi@pm.me>
2024-06-22 15:26:18 +0530

    add function

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,28 @@
 //Code here
+
+import axios from "axios";
+
+const form = document.querySelector("form");
+const addressInput = document.getElementById("address")! as HTMLInputElement;
+const Gonley = "0c008ef871bf49afaf51e3c3b267058e";
+
+function searchAddressHandler(e: Event) {
+  e.preventDefault();
+  const enteredAddress = addressInput.value;
+
+  // Send to google api
+  // Structure -https://api.geoapify.com/v1/geocode/search?text=6Th%20Avenue&format=json&apiKey=YOUR_API_KEY
+  axios
+    .get(
+      `https://api.geoapify.com/v1/geocode/search?text=${encodeURI(
+        enteredAddress
+      )}&format=json&apiKey=${Gonley}`
+    )
+    .then((res) => {
+      console.log(res);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
+form?.addEventListener("submit", searchAddressHandler);
