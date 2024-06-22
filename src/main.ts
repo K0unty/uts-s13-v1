9e0e3644b8b288a3320eb40ae95a353577503fb5
K0unty <kountiapi@pm.me>
2024-06-22 16:58:58 +0530

    addgeoapify , types

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,18 @@ const form = document.querySelector("form");
 const addressInput = document.getElementById("address")! as HTMLInputElement;
 const Gonley = "0c008ef871bf49afaf51e3c3b267058e";
 
+// Creating a customt type for the geoapify app
+type GeoApFy = {
+  results: {
+    bbox: {
+      lat1: number;
+      lat2: number;
+      lon1: number;
+      lon2: number;
+    };
+  }[];
+};
+
 function searchAddressHandler(e: Event) {
   e.preventDefault();
   const enteredAddress = addressInput.value;
@@ -13,12 +25,14 @@ function searchAddressHandler(e: Event) {
   // Send to google api
   // Structure -https://api.geoapify.com/v1/geocode/search?text=6Th%20Avenue&format=json&apiKey=YOUR_API_KEY
   axios
-    .get(
+    .get<GeoApFy>(
       `https://api.geoapify.com/v1/geocode/search?text=${encodeURI(
         enteredAddress
       )}&format=json&apiKey=${Gonley}`
     )
     .then((res) => {
+      const coordinates = res.data.results[0].bbox;
+      console.log(coordinates);
       console.log(res);
     })
     .catch((err) => {
